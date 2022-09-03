94b27e2510af3a67c8f01d84cbdf7dc7299e5c31
Salma Chtourou <108493792+SalmaChtouroudev@users.noreply.github.com>
2022-09-03 13:43:03 +0100

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -33,21 +33,32 @@ suite('Functional Tests', function () {
     });
     // #3
     test('Send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
+     chai.request(server)
         .put('/travellers')
-
+        .send({
+           surname: "Colombo",
+            })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai.request(server)
+         .put('/travellers')
+         .send({
+           surname: "da Verrazzano",
+         })
+         .end(function(err, res) {
+             assert.equal(res.status, 200);
+             assert.equal(res.type, 'application/json');
+             assert.equal(res.body.name, 'Giovanni');
+             assert.equal(res.body.surname, 'da Verrazzano');
+            done();
     });
   });
 });
