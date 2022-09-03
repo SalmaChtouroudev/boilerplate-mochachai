9333efd0780cd22fd8c68a468f91b1dc1b9053ee
Salma Chtourou <108493792+SalmaChtouroudev@users.noreply.github.com>
2022-09-03 13:42:08 +0100

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -33,14 +33,17 @@ suite('Functional Tests', function () {
     });
     // #3
     test('Send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
+      chai.request(server)
         .put('/travellers')
-
+        .send({
+           surname: "Colombo",
+            })
         .end(function (err, res) {
-          assert.fail();
-
-          done();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
+          done();;
         });
     });
     // #4
